9c2bbfdde3b53d6176ed98e83562b3d9769a1f1f
kapil gupta <kgupta@sangoma.com>
2018-05-07 16:00:38 +0530

    [Module Tag script: soundlang 13.0.24.7] Fixing FREEPBX-17433 Whoops error due to typo error in sprintf

--- vendor/composer/autoload_classmap.php
+++ vendor/composer/autoload_classmap.php
@@ -6,4 +6,12 @@ $vendorDir = dirname(dirname(__FILE__));
 $baseDir = dirname($vendorDir);
 
 return array(
+    'splitbrain\\PHPArchive\\Archive' => $vendorDir . '/splitbrain/php-archive/src/Archive.php',
+    'splitbrain\\PHPArchive\\ArchiveCorruptedException' => $vendorDir . '/splitbrain/php-archive/src/Archive.php',
+    'splitbrain\\PHPArchive\\ArchiveIOException' => $vendorDir . '/splitbrain/php-archive/src/Archive.php',
+    'splitbrain\\PHPArchive\\ArchiveIllegalCompressionException' => $vendorDir . '/splitbrain/php-archive/src/Archive.php',
+    'splitbrain\\PHPArchive\\FileInfo' => $vendorDir . '/splitbrain/php-archive/src/FileInfo.php',
+    'splitbrain\\PHPArchive\\FileInfoException' => $vendorDir . '/splitbrain/php-archive/src/FileInfo.php',
+    'splitbrain\\PHPArchive\\Tar' => $vendorDir . '/splitbrain/php-archive/src/Tar.php',
+    'splitbrain\\PHPArchive\\Zip' => $vendorDir . '/splitbrain/php-archive/src/Zip.php',
 );
