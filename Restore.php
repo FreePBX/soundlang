61039e68a27359a4da6e9c8eb04246ae6a1594a7
Franck Danard <fdanard@sangoma.com>
2019-06-19 10:44:14 +0200

    FREEPBX-20107 Sound Languages module gives exception error on restore when the system is not connected to internet

--- Restore.php
+++ Restore.php
@@ -2,13 +2,31 @@
 namespace FreePBX\modules\Soundlang;
 use FreePBX\modules\Backup as Base;
 class Restore Extends Base\RestoreBase{
-	public function runRestore($jobid){
-		$configs = $this->getConfigs();
-		$this->addDataToTableFromArray('soundlang_customlangs', $configs['customlangs']);
-		$this->addDataToTableFromArray('soundlang_packages', $configs['packages']);
-		$this->addDataToTableFromArray('soundlang_settings', $configs['settings']);
 
-		$this->redownloadPackages();
+	public function repos_alive(){
+		$connected 	= @fsockopen("sng7.com", 80); 
+       	if ($connected){
+        	$result	= true;
+          	fclose($connected);
+		}else{
+			$result = false;
+		}
+      return $result;		
+	}
+
+	public function runRestore($jobid){
+		if($this->repos_alive()){
+			$configs = $this->getConfigs();
+			$this->addDataToTableFromArray('soundlang_customlangs', $configs['customlangs']);
+			$this->addDataToTableFromArray('soundlang_packages', $configs['packages']);
+			$this->addDataToTableFromArray('soundlang_settings', $configs['settings']);
+
+			$this->redownloadPackages();			
+		}
+		else{
+			out( "/!\\ "._("Unable to download the some dependencies for restoring. Please enable internet access and try restoring again.")." /!\\");
+		}
+
 	}
 
 	public function processLegacy($pdo, $data, $tablelist, $unknowntables){
